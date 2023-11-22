270b1c1bbb6a2ae60b5c9e2fdfd8f1bde81c0501
Bo Nicolajsen <>
2023-11-22 13:37:31 +0100

    done

--- assets/js/gallery.js
+++ assets/js/gallery.js
@@ -1,1 +1,55 @@
 // Din cool code here
+
+const myImages = ['elephant.jpg', 'great-white.jpg', 'koala.jpg', 'smithi.jpg', 'tiger.jpg'];
+
+const baseUrl = 'assets/img/';
+
+const mygallery = document.getElementById('gallery');
+
+
+CreateGallery();
+
+
+
+function createCard(myUrl, mygallery) {
+
+    let myArticle = document.createElement('article');
+    myArticle.classList.add('galleryCard');
+    let myImage = document.createElement('img');
+    myImage.src = myUrl;
+
+    myImage.addEventListener('click', (event) => {
+        console.log(myUrl);
+        ShowBigCard(myUrl);
+
+    });
+    myArticle.appendChild(myImage);
+
+    return myArticle;
+
+}
+
+function ShowBigCard(myUrl) {
+    mygallery.innerHTML = '';
+    let myArticle = document.createElement('article');
+    myArticle.classList.add('detailView');
+    let myImage = document.createElement('img');
+
+    myImage.src = myUrl;
+
+    myImage.addEventListener('click', (event) => {
+        CreateGallery();
+
+    });
+    myArticle.appendChild(myImage);
+    mygallery.appendChild(myArticle);
+}
+
+function CreateGallery() {
+    mygallery.innerHTML = '';
+    myImages.forEach(animal => {
+
+        mygallery.appendChild(createCard(baseUrl + animal));
+
+    });
+}
